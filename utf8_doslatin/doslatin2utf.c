cec0b9f2483b5ff25f8c294e93d7ff9262d25cbf
Steven Arnow <s@rdw.se>
2014-11-17 08:01:57 +0100

    Added rudimentary util to convert between UTF8 and CP437

---
+++ utf8_doslatin/doslatin2utf.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#include "lookup.h"
+
+/* Assume single-byte encoding as ingoing. If you want to adapt this to work
+** with something even sillier, like Shift-JIS, look elsewhere...		*/
+
+
+unsigned char *char_lookup(const unsigned char *str) {
+	int i;
+
+	for (i = 0; replace_arr[i*2]; i++)
+		if (!strcmp(str, replace_arr[i * 2 + 1]))
+			return replace_arr[i * 2 + 1];
+	if (*str < 0x7F)
+		return str;
+	return "?";
+}
+
+
+int main() {
+	unsigned char buff[2];
+	buff[1] = 0;
+
+	while (!feof(stdin)) {
+		buff[0] = fgetc(stdin);
+		if (feof(stdin))
+			return 0;
+		fprintf(stdout, "%s", char_lookup(buff));
+	}
+
+	return 0;
+}
---
+++ utf8_doslatin/utf2doslatin.c
@@ -0,0 +1,48 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "lookup.h"
+
+unsigned char *char_lookup(const unsigned char *str) {
+	int i;
+
+	for (i = 0; replace_arr[i*2]; i++)
+		if (!strcmp(str, replace_arr[i * 2]))
+			return replace_arr[i * 2];
+	if (*str < 0x7F)
+		return str;
+	return "?";
+}
+
+
+int main() {
+	unsigned char buff[8];
+	int left, tmp;
+
+	while (!feof(stdin)) {
+		buff[0] = fgetc(stdin);
+		if (feof(stdin))
+			return 0;
+		if (buff[0] < 0x80) {
+			buff[1] = 0;
+			fprintf(stdout, "%s", char_lookup(buff));
+			continue;
+		} else {
+			if ((buff[0] & 0xC0) == 0x80) {	/* Bad continuation byte */
+				fputc('?', stdout);
+				continue;
+			}
+
+			tmp = buff[0] << 1;
+			for (left = 0; tmp & 0x80; tmp <<= 1, left++);
+			for (tmp = 0; tmp < left; tmp++)
+				buff[1 + tmp] = fgetc(stdin);
+			buff[1 + tmp] = 0;
+			fprintf(stdout, "%s", char_lookup(buff));
+		}
+		buff[0] = 0;
+	}
+			
+	return 0;
+}
